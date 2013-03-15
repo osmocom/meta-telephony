7aa59c430eedbd2ac0f6ca5a5b87aa6052e8d233
Holger Hans Peter Freyther <zecke@selfish.org>
2013-03-15 22:34:50 +0100

    gsmd: Work on stopping the respawn script and children
    This is using some brute force. Make the shell script write a PID
    file and on stop kill the respawn script and then all process names
    that are spawned. For the stop we need to know that this is a bash
    script.

--- recipes-openmoko/openmoko-gsmd/files/gsmd-respawn.sh
+++ recipes-openmoko/openmoko-gsmd/files/gsmd-respawn.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+NAME=`basename $1`
+PIDFILE="/var/run/$NAME.pid"
+PID=$$
+echo "$PID" > $PIDFILE
+
+trap "{ rm -f $PIDFILE ; killall -9 $NAME; }" EXIT
+
 while true;
 do
 	echo "Restarting not dealing well with spaces.."
